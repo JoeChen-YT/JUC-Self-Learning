d3fbcae5769402ea9cb9b97a9acc00a656bfe39d
Zhuo Chen <36575683+JoeChen-YT@users.noreply.github.com>
2024-07-04 23:13:38 -0700

    Initial commit for JUC experiments

---
+++ app/src/main/java/joe/experiment/juc/thread/basic/JavaThreadCreation.java
@@ -0,0 +1,40 @@
+package joe.experiment.juc.thread.basic;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class JavaThreadCreation {
+    private static final String MAIN_THREAD_LOG_MESSAGE = "Main thread is running";
+    private static final String CHILD_THREAD_LOG_MESSAGE = "I'm in the child thread";
+
+    public static void main(String[] args) {
+        final Thread childThread = threadCreationViaRunnableInLambda();
+
+        childThread.start();
+        log.info(MAIN_THREAD_LOG_MESSAGE);
+    }
+
+    private static Thread threadCreation() {
+        return new Thread("ChildThread") {
+            @Override
+            public void run() {
+                log.info(CHILD_THREAD_LOG_MESSAGE);
+            }
+        };
+    }
+
+    private static Thread threadCreationViaRunnable() {
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                log.info(CHILD_THREAD_LOG_MESSAGE);
+            }
+        };
+        return new Thread(runnable, "ChildThread");
+    }
+    private static Thread threadCreationViaRunnableInLambda() {
+        return new Thread(() -> {log.info(CHILD_THREAD_LOG_MESSAGE);},"ChildThread");
+
+    }
+
+}
